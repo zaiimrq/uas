4d49e0941e7be99467410a466dcbaeeccc94188c
zaiimrq <zaiimrq@gmail.com>
2023-12-19 14:19:40 +0900

    fixx

--- config/global.php
+++ config/global.php
@@ -4,10 +4,18 @@ use Tugas\core\Route;
 
 
 $uri = $_SERVER["REQUEST_URI"];
-$uri = rtrim($uri, "/");
 
-if (!Route::is("/")) {
+
+if (!Route::is('/')) {
 
     $uri = strstr($uri, "/dashboard", true);
 }
+
+if (isset($_GET["search"])) {
+    $uri = strstr($uri, "/?", true);
+}
+$uri = "http://" . $_SERVER["HTTP_HOST"] . $uri;
+
+$uri = rtrim($uri, '/');
+
 define("BASEURL", $uri);
